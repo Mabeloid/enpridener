b312fdcb6aefc5607a2c6399829f3efb361039ca
Mabeloid <45720989+Mabeloid@users.noreply.github.com>
2025-01-03 22:50:16 +0100

    moved example from main to example

--- enpridener.py
+++ enpridener.py
@@ -106,14 +106,3 @@ class Prider:
     def save(self, *args):
         self.image.save(*args)
         return self
-
-
-if __name__ == "__main__":
-    p = Prider(filepath="icon_in.png", crop=None)
-    p.pride("trans", radius=0.5)
-    p.pride("nonbinary", radius=0.5)
-    p.pride("lesbian", radius=0.5)
-    p.pride("ace", radius=0.5)
-    p.pride("aro", radius=0.5)
-    p.save("icon_out.png")
-    p.resize(512).save("icon_out_resized.png")
--- example.py
+++ example.py
@@ -1,4 +1,10 @@
 from enpridener import Prider
 
-p = Prider(filepath=r"C:\Users\mabel\Pictures\wolfie\lmabel.png", crop=None)
-p.pride("nonbinary", 0.5).save(r"C:\Users\mabel\Pictures\wolfie\flagged\lmabel.png")
+p = Prider(filepath="icon_in.png", crop=None)
+p.pride("trans", radius=0.5)
+p.pride("nonbinary", radius=0.5)
+p.pride("lesbian", radius=0.5)
+p.pride("ace", radius=0.5)
+p.pride("aro", radius=0.5)
+p.save("icon_out.png")
+p.resize(512).save("icon_out_resized.png")
