5eeedf8dcb3b6034c1e4f3aef4a7e615ff362bfd
Mabeloid <45720989+Mabeloid@users.noreply.github.com>
2025-01-03 22:43:59 +0100

    include example

---
+++ example.py
@@ -0,0 +1,4 @@
+from enpridener import Prider
+
+p = Prider(filepath=r"C:\Users\mabel\Pictures\wolfie\lmabel.png", crop=None)
+p.pride("nonbinary", 0.5).save(r"C:\Users\mabel\Pictures\wolfie\flagged\lmabel.png")
